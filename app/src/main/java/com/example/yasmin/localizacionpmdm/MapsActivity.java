13408285035e1f8e28823df84c9317077dd2d761
Yasmin <ylagorebollar@danielcastelao.org>
2017-01-27 12:03:17 +0100

    Creados los permisos de localización Ubicación actual del usuario Integración de control zoom

--- app/src/main/java/com/example/yasmin/localizacionpmdm/MapsActivity.java
+++ app/src/main/java/com/example/yasmin/localizacionpmdm/MapsActivity.java
@@ -1,15 +1,15 @@
 package com.example.yasmin.localizacionpmdm;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
+import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -17,14 +17,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private GoogleMap mMap;
     private FirstMapFragment mFirstMapFragment;
-
+    private static final int PERMISO = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
         //Creacion fragmento de forma dinamica
         mFirstMapFragment = FirstMapFragment.newInstance();
         getSupportFragmentManager()
@@ -34,28 +33,75 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mFirstMapFragment.getMapAsync(this);//Asocia la actividad como escucha
     }
 
-
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Este método es llamado cuando el mapa está listo para ser utilizado
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng vigo = new LatLng(42.2154941 , -8.8120584);
-        mMap.addMarker(new MarkerOptions()
-                .position(vigo)
-                .title("Aquí estoy yo"));
-        CameraPosition cameraPosition = CameraPosition.builder()
-                .target(vigo)
-                .zoom(10)
-                .build();
-
-        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        solicitarPermiso();
+        mMap.getUiSettings().setZoomControlsEnabled(true); //Controles integrados de zoom
+        // Marcadores
+        mMap.addMarker(new MarkerOptions().position(new LatLng(42.1416, -8.4324)));
     }
+
+    /***********************************PERMISOS DE LOCALIZACION***********************************/
+    /**
+     * Comprueba los permisos y los concede
+     * Muestra un mensaje en tiempo de ejecución explicando la necesidad de dichos permisos
+     */
+    public void solicitarPermiso(){
+        int checkPerm = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (checkPerm == PackageManager.PERMISSION_GRANTED) { //Concede el permiso para la localización
+            mMap.setMyLocationEnabled(true);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Aquí se muestra el diálogo explicativo de porque se necesitan los permisos
+            } else {
+                // Se solicitan los permisos
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISO);
+            }
+        }
+    }
+
+    /**
+     * Devuelve la llamada para el resultado de solicitar permisos.
+     * Se invoca a través del método requestPermission()
+     * @param requestCode Código de solicitud devuelto por requestPermission()
+     * @param permissions Permisos solicitados. Nunca puede ser nulo
+     * @param grantResults Resultado de la conexión para los permisos
+     *                     correspondientes.(PERMISSION_GRANTED o PERMISSION_DENIED)Nunca nulo
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISO) {
+            // ¿Permisos asignados?
+            if (permissions.length > 0 &&
+                    permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                /*
+                Al habilitar la capa MyLocation aparece en la parte superior derecha un botón de localización.
+                Al pulsarlo, la camara centra el mapa en la ubicación actual.
+                La ubicación se muestra a través de un punto azul.
+                 */
+                mMap.setMyLocationEnabled(true);
+            } else {
+                Toast.makeText(this, "Error de permisos", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    /*******************************FIN PERMISOS LOCALIZACION**************************************/
 }
